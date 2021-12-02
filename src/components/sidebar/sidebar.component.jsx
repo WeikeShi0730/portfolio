a172bbb45050921cd4c3619bb13d6c074fc05292
Weike Shi <weikeshih@gmail.com>
2021-12-01 22:10:03 -0500

    added components files

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -9,9 +9,12 @@ import { HiOutlineMail } from "react-icons/hi";
 const Sidebar = () => {
   return (
     <div
-      className="fixed top-0 left-0 h-screen w-16 flex flex-col
+      className="fixed top-0 left-0 h-screen w-24 flex flex-col
     bg-white dark:bg-gray-900 shadow-lg"
     >
+      <div className="flex items-center justify-center h-24 bg-gray-400 text-green-500 text-3xl">
+        WS
+      </div>
       <div className="space-y-10 absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2">
         <SidebarIcon icon={<BiHomeAlt />} text="home"></SidebarIcon>
         <SidebarIcon
