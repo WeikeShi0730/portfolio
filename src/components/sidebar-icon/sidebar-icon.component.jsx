c21f14c5e9421ed54f4ea8b8f871319527ef3708
Weike Shi <weikeshih@gmail.com>
2021-12-07 21:54:29 -0500

    new styles

--- src/components/sidebar-icon/sidebar-icon.component.jsx
+++ src/components/sidebar-icon/sidebar-icon.component.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { IconContext } from "react-icons";
 
 const SidebarIcon = ({ icon, text }) => {
   return (
     <div className="sidebar-icon group">
-      {icon}
+      <IconContext.Provider
+        value={{ size: "1.5rem"}}
+      >
+        <div>{icon}</div>
+      </IconContext.Provider>
       <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
     </div>
   );
