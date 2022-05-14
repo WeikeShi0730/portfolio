25f9ce42a06bec0a0871651d2ea107e13035a829
Weike Shi <weikeshih@gmail.com>
2022-05-13 23:48:37 -0400

    Update footer

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="p-2 bg-slate-200 bg-opacity-80 dark:bg-neutral-800 dark:bg-opacity-80 backdrop-blur-sm">
       <div className="w-full mx-0 my-auto text-center sm:text-lg text-md font-extralight text-black dark:text-white">
-        © Weike Shi 2021
+        © Weike Shi 2022
       </div>
     </div>
   );
